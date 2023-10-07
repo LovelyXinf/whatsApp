3da58db338a35aaa18a1ea3ea50313dca89584c4
LovelyXinf <146376679+LovelyXinf@users.noreply.github.com>
2023-10-07 06:48:02 -0700

    d

--- src/Request/MessageReadRequest.php
+++ src/Request/MessageReadRequest.php
@@ -26,8 +26,7 @@ final class MessageReadRequest extends Request implements RequestWithBody
     }
 
     /**
-     * Returns the raw body of the request.
-     *
+
      * @return array
      */
     public function body(): array
